6ffee9b1fa8bb8ffb8d690d52fa8f784fb6ea14d
joseph <joseph.aviles.dalton@gmail.com>
2024-09-29 22:47:02 -0500

    infinite loop, loop that doesnt loop & breaking out of a look examples added

--- loops 1.py
+++ loops 1.py
@@ -7,3 +7,29 @@ while n > 0:
 
 print('blastoff!')
 print(n)
+
+""" infinite loop example """
+
+n = 5
+while n > 0:
+    print('Lather')
+    print('Rinse')
+print('Dry off!')
+
+""" loop that doesnt loop """
+
+n = 0
+while n > 0 :
+    print('lather')
+    print('rinse')
+print('dry off!')
+
+""" breaking out of a loop """
+
+while True:
+    line = input('> ')
+    if line == 'done' :
+        break
+    print(line)
+print('Done!')
+
