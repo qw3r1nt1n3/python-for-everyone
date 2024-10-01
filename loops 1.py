7c1cab5e97e47eefcf354f1d6f2adfdc97247c6b
joseph <joseph.aviles.dalton@gmail.com>
2024-09-30 22:17:43 -0500

    added definite loop with strings

--- loops 1.py
+++ loops 1.py
@@ -52,3 +52,9 @@ for i in [5, 4, 3, 2, 1] :
     print(i)
 print('blastoff!')
 
+""" definite loop with strings """
+
+friends = ['joseph', 'glenn','sally']
+for friend in friends :
+    print('happy new year:', friend)
+print('done!')
