cde231f0937237c6c364080e9f1cf232c1e4d582
joseph <joseph.aviles.dalton@gmail.com>
2024-09-30 22:54:39 -0500

    added finding the largest value example

--- loops 1.py
+++ loops 1.py
@@ -68,3 +68,15 @@ for thing in [9, 41, 12, 3, 74, 15] :
 
 print('after')
 
+""" finding the largest value """
+
+largest_so_far = -1
+
+print('before', largest_so_far)
+
+for the_num in [9, 41, 12, 3, 74, 15] :
+    if the_num > largest_so_far :
+        largest_so_far = the_num
+    print(largest_so_far, the_num)
+
+print('after', largest_so_far)
